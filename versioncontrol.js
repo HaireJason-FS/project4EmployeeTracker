f8a138cf86a8d650e97bc8d7e4b29a63179504d5
Jason Haire <cjhaire@student.fullsail.edu>
2025-10-26 12:35:07 -0400

    Added Hard coded employees and main code for Javascript for testing

--- versioncontrol.js
+++ versioncontrol.js
@@ -48,3 +48,132 @@ ID  Name	Age	Salary	Hours	 Pay Rate	 Manager/Part-Time
 
 We will be using the string split() method to quickly create an array based off the values in a single string.
 */
+
+class Employee {
+  constructor(n, a){
+    this.name = n;
+    this.age = a;
+    this.annualSalary = 0;
+    this.weeklyHours = 0;
+    this.payRate = 0;
+  }
+}
+
+class Manager extends Employee {
+  constructor(p,n,a){
+    super(n, a);
+    this.weeklyHours = 40;
+    this.payRate = p;
+    this.type = "Manager";
+    this.calculatePay();
+  }
+  calculatePay() {
+    this.annualSalary = (this.weeklyHours * this.payRate * 52) - 1000;
+  }
+}
+
+class PartTime extends Employee {
+  constructor(p,h,n,a){
+    super(n, a);
+    this.weeklyHours = h;
+    this.payRate = p;
+    this.type = "Part-Time";
+    this.calculatePay();
+  }
+  calculatePay() {
+    this.annualSalary = this.weeklyHours * this.payRate * 52;
+  }
+}
+
+//Main Class
+class EmpTracker{
+  constructor(){}
+
+  displayMenu(){
+    let choice = Number(prompt("Main Menu\n1. Add Employee\n2. Remove Employee\n3. Edit Employee\n4. Display Employees\n5. Exit\n\nEnter your choice (1-5):"));
+
+    switch(choice){
+      case 1:
+        this.addEmployee();
+        break;
+      case 2:
+        this.removeEmployee();
+        break;
+      case 3:
+        this.editEmployee();
+        break;
+      case 4:
+        this.displayEmployees();
+        break;
+      case 5:
+        console.log("Exiting program.");
+        break;
+      default:
+        alert("Invalid choice. Please enter a number between 1 and 5.");
+    }
+    this.displayMenu();
+  }
+
+  addEmployee(){
+    let empString = prompt("Enter Employee Information (Name, Age, Pay Rate, Weekly Hours) separated by commas:");
+    let empArray = empString.split(",").map(item => item.trim());
+    let name = empArray[0];
+    let age = Number(empArray[1]);
+    let payRate = Number(empArray[2]);
+    let weeklyHours = Number(empArray[3]);
+    let newEmployee;
+
+    if(weeklyHours >= 40){
+      newEmployee = new Manager(payRate, name, age);
+    } else {
+      newEmployee = new PartTime(payRate, weeklyHours, name, age);
+    }
+    this.employees.push(newEmployee);
+  }
+  removeEmployee(){
+    let empToRemove = prompt("Enter Employee Number or Name to Remove:");
+    if(!isNaN(empToRemove)){
+      let empIndex = Number(empToRemove) - 1;
+      if(empIndex >= 0 && empIndex < this.employees.length){
+        this.employees.splice(empIndex, 1);
+      } else {
+        alert("Employee number not found.");
+      }
+    } else {
+      this.employees = this.employees.filter(emp => emp.name.toLowerCase() !== empToRemove.toLowerCase());
+    }
+  }
+  editEmployee(){
+    let empNumber = Number(prompt("Enter Employee Number to Edit Pay Rate:")) - 1;
+    if(empNumber >= 0 && empNumber < this.employees.length){
+      let newPayRate = Number(prompt("Enter New Pay Rate:"));
+      this.employees[empNumber].payRate = newPayRate;
+      this.employees[empNumber].calculatePay();
+    } else {
+      alert("Employee number not found.");
+    }
+  }
+
+  displayEmployees(){
+    if(this.employees.length === 0){
+      console.log("No employees to display.");
+      return;
+    }
+    console.log("ID\tName\tAge\tSalary\tHours\t Pay Rate\t Manager/Part-Time");
+    this.employees.forEach((emp, index) => {
+      console.log(`${index + 1}\t${emp.name}\t${emp.age}\t${emp.annualSalary}\t${emp.weeklyHours}\t${emp.payRate}\t${emp.type}`);
+    });
+  }
+}
+//Hard coded employees (need to be displayed in the console when program starts)
+(e=>{
+  e.employees = [
+    new Manager(10, "Scott", 25),
+    new Manager(5, "Dave", 30),
+    new PartTime(8, 12, "Lisa", 22)
+  ];
+  console.clear();
+  e.displayEmployees();
+  e.displayMenu();
+  const empTracker = new EmpTracker();
+})(new EmpTracker());
